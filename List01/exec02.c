7dde856216550b9cb31d2676eb8c652621a83ad3
RenanGAS <renansakaoki@gmail.com>
2021-06-28 13:32:48 -0300

    Adding exec02.c

---
+++ List01/exec02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("Bem vindo a codificação em C/C++!\n");
+    printf("Para criar programas, você precisa de um editor de texto e um compilador.\n");
+    printf("O compilador GCC (GNU Compiler Collection) é muito utilizado para o desenvolvimento de software.\n");
+    printf("Ele inclui front ends para as linguagens C, C++, Objective-C, Fortran, Ada e Go.\n");
+
+    return 0;
+}
