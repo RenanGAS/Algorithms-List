2f906c432f4ab406c2e4b5d8a9a36f648b6ce3d6
RenanGAS <renansakaoki@gmail.com>
2021-06-28 13:47:30 -0300

    Adding exec03.c

---
+++ List01/exec03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int varInt = 3;
+    float varFloat = 0.3;
+    char varChar = 'a';
+
+    printf("Valor do número do tipo \"int\": %d\n", varInt);
+    printf("Valor do número do tipo \"float\": %f\n", varFloat);
+    printf("Valor do caracter: %c\n", varChar);
+
+    return 0;
+}
