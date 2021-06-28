d76e8598805ba55361c9890cbec06befd863d0d2
RenanGAS <renansakaoki@gmail.com>
2021-06-28 16:15:08 -0300

    Adding exec04.c

---
+++ List01/exec04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char frase[10] = "The box";
+
+    printf("+-------------+\n");
+    printf("|             |\n");
+    printf("|   %s   |\n", frase);
+    printf("|             |\n");
+    printf("+-------------+\n");
+
+    return 0;
+}
