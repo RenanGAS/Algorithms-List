289beead7ec7ad15d3afd39268516d2d696ffe34
RenanGAS <renansakaoki@gmail.com>
2021-07-08 12:48:24 -0300

    Adding Exec04

---
+++ List02/Exec04/exec04.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int varInt = 0;
+    int i;
+    int j = 2;
+
+    printf("\n\t\tTabuada\n\n");
+    printf("Digite um número: ");
+    scanf(" %d", &varInt);
+    printf("\nTabuada do número %d:\n\n", varInt);
+
+    for (i = 1; i < 6; i++)
+    {
+        int i1 = i+5;
+        printf("%d x %d = %d %5d x %d = %d\n", varInt, i, varInt*i, varInt, i1, varInt*i1);
+    }
+
+    return 0;
+}
