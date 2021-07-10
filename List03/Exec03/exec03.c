fe5d5eb7bf454fe218f87c8dc9f265baba0e1d63
RenanGAS <renansakaoki@gmail.com>
2021-07-09 21:03:35 -0300

    Adding Exec03

---
+++ List03/Exec03/exec03.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+    int n1 = 7;
+    int n2 = 8;
+
+    printf("\nDados os números %d e %d, temos que:\n", n1, n2);
+
+    if (n1>=0)
+    {
+        if (n2>=0)
+        {
+            if (n1!=n2)
+            {
+                if (n1 > n2)
+                {
+                    printf("\n=> %d é maior que %d!\n\n", n1, n2);
+                } else
+                {
+                    printf("\n=> %d é maior que %d!\n\n", n2, n1);
+                }
+            } else 
+            {
+                printf("\n=> %d é igual a %d!\n\n", n1, n2);
+            }
+        } else
+        {
+            printf("\n=> O número %d não é um número natural!\n\n", n2);
+        }
+    } else
+    {   
+        printf("\n=> O número %d não é um número natural!\n\n", n1);
+    }
+
+    return 0;
+}
