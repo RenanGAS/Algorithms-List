d077c77b6590364da5140045b6a72ba399a96e2b
RenanGAS <renansakaoki@gmail.com>
2021-07-10 12:49:21 -0300

    Adding Exec06

---
+++ List03/Exec06/exec06.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 3;
+    int c = 5;
+    int d = 2;
+    int array[4] = {a, b, c, d};
+    int arraysize = sizeof(array)/4;
+    int newsize = arraysize;
+    int i;
+    int j;
+    int val = 0;
+    int valnext = 0;
+
+    printf("\nDados os números (%d, %d, %d, %d), temos que:\n", array[0], array[1], array[2], array[3]);
+
+    for (i = 0; i < arraysize-1; i++)
+    {
+        for (j = 0; j < newsize; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                val = array[j];
+                valnext = array[j+1];
+                array[j + 1] = val;
+                array[j] = valnext;
+            }
+        }
+        newsize -= 1;
+    }
+
+    printf("\n=> Em ordem crescente, obtemos a sequência: (%d, %d, %d, %d)\n", array[0], array[1], array[2], array[3]);
+    printf("=> Sendo o maior número: %d\n\n", array[3]);
+
+    return 0;
+}
