8a79204475f3601f7bc0cfbdd194a59960c9102f
RenanGAS <renansakaoki@gmail.com>
2021-07-11 10:05:26 -0300

    Updating Exec06

--- List03/Exec06/exec06.c
+++ List03/Exec06/exec06.c
@@ -2,36 +2,33 @@
 
 int main()
 {
-    int a = 9;
-    int b = 10;
-    int c = 0;
-    int d = 1;
+    int a = 13;
+    int b = 15;
+    int c = 14;
+    int d = 16;
     int array[4] = {a, b, c, d};
-    int arraysize = sizeof(array)/4;
-    int newsize = arraysize;
+    int arraysize = sizeof(array) / 4;
     int i;
     int j;
     int val;
     int valnext;
+    int result;
 
     printf("\nDados os números (%d, %d, %d, %d), temos que:\n", array[0], array[1], array[2], array[3]);
 
-    for (i = 0; i < arraysize-1; i++)
+    for (j = 0; j < arraysize; j++)
     {
-        for (j = 0; j < newsize; j++)
+        if (array[j] > array[j + 1])
         {
-            if (array[j] > array[j + 1])
-            {
-                val = array[j];
-                valnext = array[j+1];
-                array[j + 1] = val;
-                array[j] = valnext;
-            }
+            val = array[j];
+            valnext = array[j + 1];
+            array[j + 1] = val;
+            array[j] = valnext;
         }
-        newsize -= 1;
     }
 
-    printf("\n=> O maior número é: %d\n\n", array[3]);
+    result = array[3];
+    printf("\n=> O maior número é: %d\n\n", result);
 
     return 0;
 }
