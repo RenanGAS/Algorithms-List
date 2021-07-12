dd760346c33c8c2a4150dff1316a0bac4ab930de
RenanGAS <renansakaoki@gmail.com>
2021-07-12 15:42:52 -0300

    Updating Exec04

--- List02/Exec04/exec04.c
+++ List02/Exec04/exec04.c
@@ -2,14 +2,13 @@
 
 int main(){
     int varInt = 0;
-    int i;
 
     printf("\n\t\tTabuada\n\n");
     printf("Digite um número: ");
     scanf(" %d", &varInt);
     printf("\nTabuada do número %d:\n\n", varInt);
 
-    for (i = 1; i < 6; i++)
+    for (int i = 1; i < 6; i++)
     {
         int i1 = i+5;
         printf("%d x %d = %d %5d x %d = %d\n", varInt, i, varInt*i, varInt, i1, varInt*i1);
