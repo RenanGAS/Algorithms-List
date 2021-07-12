6c3a5d776b5734baad9056cbfc5869696cfec7ad
RenanGAS <renansakaoki@gmail.com>
2021-07-12 15:41:04 -0300

    Updating Exec06

--- List03/Exec06/exec06.c
+++ List03/Exec06/exec06.c
@@ -8,22 +8,20 @@ int main()
     int d = 16;
     int array[4] = {a, b, c, d};
     int arraysize = sizeof(array) / 4;
-    int i;
-    int j;
     int val;
     int valnext;
     int result;
 
     printf("\nDados os números (%d, %d, %d, %d), temos que:\n", array[0], array[1], array[2], array[3]);
 
-    for (j = 0; j < arraysize; j++)
+    for (int i = 0; i < arraysize; i++)
     {
-        if (array[j] > array[j + 1])
+        if (array[i] > array[i + 1])
         {
-            val = array[j];
-            valnext = array[j + 1];
-            array[j + 1] = val;
-            array[j] = valnext;
+            val = array[i];
+            valnext = array[i + 1];
+            array[i + 1] = val;
+            array[i] = valnext;
         }
     }
 
