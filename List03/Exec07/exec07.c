8436f95070b85e430cbc6d10f5944457b22f961c
RenanGAS <renansakaoki@gmail.com>
2021-07-14 14:31:15 -0300

    Updating Exec07

--- List03/Exec07/exec07.c
+++ List03/Exec07/exec07.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    int a = 5;
-    int b = 6;
-    int c = 3;
+    int a = 8;
+    int b = 9;
+    int c = 7;
     int array[3] = {a, b, c};
     int arraysize = sizeof(array)/4;
     int newsize = arraysize;
@@ -14,14 +14,13 @@ int main(){
 
     for (int i = 0; i < arraysize-1; i++)
     {
-        for (int j = 0; j < newsize; j++)
+        for (int j = 1; j < newsize; j++)
         {
-            if (array[j] > array[j + 1])
+            if (array[j] < array[j - 1])
             {
-                val = array[j];
-                valnext = array[j+1];
-                array[j + 1] = val;
-                array[j] = valnext;
+                valnext = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = valnext;
             }
         }
         newsize -= 1;
