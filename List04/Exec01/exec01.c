468db561a7fdca2e4911b9c62dd4ce2acaf90268
RenanGAS <renansakaoki@gmail.com>
2021-07-19 14:30:29 -0300

    Adding Exec01

---
+++ List04/Exec01/exec01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int varInt = 0;
+    printf("\nInforme o número de linhas: ");
+    scanf(" %d", &varInt);
+
+    for (int i = 1; i <= varInt; i++)
+    {
+        printf("\nLinha %d\n", i);
+    }
+
+    printf("\n");
+
+    return 0;
+}
