dd9f56c2f549b0a14cf0efaabf22d87ed5751bbf
RenanGAS <renansakaoki@gmail.com>
2021-07-20 19:16:27 -0300

    Adding Exec06

---
+++ List04/Exec06/exec06.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int varInt = 0;
+    int cont = 0;
+
+    printf("\nInforme o número: ");
+    scanf(" %d", &varInt);
+    printf("\n");
+
+    for (int i = 1; i <= varInt; i++)
+    {
+        if (i != varInt)
+        {
+            cont += i;
+            printf("%d + ", i);
+        } else
+        {
+            cont += i;
+            printf("%d = %d\n\n", i, cont);
+        }
+    }
+
+    if (varInt == 0)
+    {
+        printf("%d = %d\n\n", varInt, varInt);
+    }
+    
+
+    return 0;
+}
