a97b9c3af7909916015d85e430fa7e9dbef8edc8
RenanGAS <renansakaoki@gmail.com>
2021-07-20 21:39:04 -0300

    Adding Exec09

--- List04/Exec09/exec09.c
+++ List04/Exec09/exec09.c
@@ -2,6 +2,43 @@
 
 int main()
 {
-    
+    int b = 0;
+    int s = 0;
+    int varInt = 0;
+    int verif = 1;
+
+    printf("\nInforme os números:\n");
+
+    while (verif)
+    {
+        printf("\n> ");
+        scanf(" %d", &varInt);
+
+        if (varInt != 0)
+        {
+            if (varInt > b)
+            {
+                b = varInt;
+            }
+
+            if (s == 0)
+            {
+                s = varInt;
+            }
+            else
+            {
+                if (varInt < s)
+                {
+                    s = varInt;
+                }
+            }
+        }
+        else
+        {
+            printf("\nMaior: %d\nMenor: %d\n\n", b, s);
+            verif = 0;
+        }
+    }
+
     return 0;
 }
