b72926897131f1272544ce0e0956aee0ee9658a7
RenanGAS <renansakaoki@gmail.com>
2021-08-02 15:03:25 -0300

    Adding List05

---
+++ List05/Exec01/exec01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int is_letter(char ch)
+{
+    int r = 0;
+
+    if ((ch >= 65 && ch <= 90) || (ch >= 97) && (ch <= 122))
+    {
+        r = 1;
+    }
+
+    return r;
+}
+
+int main()
+{
+    char varChar = ' ';
+    int r = 0;
+
+    printf("\nDigite um caracter: ");
+    scanf(" %c", &varChar);
+
+    r = is_letter(varChar);
+
+    printf("\n=> %d\n\n", r);
+
+    return 0;
+}
