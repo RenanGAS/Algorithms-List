b8dc4af3d33976bb95e3359cad65151f62d89243
RenanGAS <renansakaoki@gmail.com>
2021-08-03 13:31:37 -0300

    Updating Exec02

--- List05/Exec02/exec02.c
+++ List05/Exec02/exec02.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
-int main(){
+int is_prime(int n)
+{
+    int r = 1;
+
+    for (int i = 2; i < 10; i++)
+    {
+        if (n == i)
+        {
+            for (int j = 2; j < 10; j++)
+            {
+                if (n != i)
+                {
+                    if (n % i == 0)
+                    {
+                        r = 0;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (n % i == 0)
+            {
+                r = 0;
+            }
+        }
+    }
+
+    return r;
+}
+
+int main()
+{
+    int varInt = 0;
+
+    printf("\nDigite um número: ");
+    scanf(" %d", &varInt);
+
+    printf("\n=> %d\n\n", is_prime(varInt));
 
     return 0;
 }
