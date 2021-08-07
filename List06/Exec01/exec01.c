9fb4e11a7ebcc79c1934452c614f5c35424c60bd
RenanGAS <renansakaoki@gmail.com>
2021-08-07 18:42:15 -0300

    Adding Exec01

--- List06/Exec01/exec01.c
+++ List06/Exec01/exec01.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+void printString(char str[])
+{
+    printf("\n=> \"String\" digitada: ");
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        printf("%c", str[i]);
+    }
+
+    printf("\n\n");
+    
+}
+
 int main()
 {
+    char string[] = " ";
+
+    printf("\nDigite algo do tipo \"String\": ");
+    scanf(" %s", &string);
+
+    printString(string);
 
     return 0;
 }
