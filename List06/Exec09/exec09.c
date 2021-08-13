62b8031a86a9ca0a0f44bb48f40194149e5709bd
RenanGAS <renansakaoki@gmail.com>
2021-08-13 20:35:04 -0300

    Adding Exec09

--- List06/Exec09/exec09.c
+++ List06/Exec09/exec09.c
@@ -4,24 +4,36 @@
 int countWordPlus(char str[])
 {
     int r = 0;
+    int l = 0;
 
     for (int i = 0; i < strlen(str); i++)
     {
-        /* code */
+        if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122))
+        {
+            l += 1;
+        }
+        else if (l > 1)
+        {
+            r += 1;
+            l = 0;
+        }
+        else
+        {
+            l = 0;
+        }
     }
-    
 
     return r;
 }
 
 int main()
 {
-    char str[] = "  Renan Guensuke   , Aoki Sakashita     ";
+    char str[] = "  first     things  first, second   things latter ";
     int r = 0;
 
     r = countWordPlus(str);
 
-    printf("\=> Número de palavras: %d\n\n", r);
+    printf("\n=> Número de palavras: %d\n\n", r);
 
     return 0;
 }
