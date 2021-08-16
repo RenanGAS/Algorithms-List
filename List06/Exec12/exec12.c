dd2e7b06123a4ee26b2c89fe33daeb814b0061ec
RenanGAS <renansakaoki@gmail.com>
2021-08-16 13:48:57 -0300

    To fix

--- List06/Exec12/exec12.c
+++ List06/Exec12/exec12.c
@@ -3,12 +3,29 @@
 
 void myStringTrim(char str[])
 {
-    
+    char aux = ' ';
+    int point = 0;
+
+    for (int i = 0; i < strlen(str); i++)
+    {
+        for (int j = i + 1; j < strlen(str); j++)
+        {
+            if (str[i] == 32)
+            {
+                if (str[j] != 32)
+                {
+                    aux = str[j];
+                    str[j] = 32;
+                    str[i] = aux;
+                }
+            }
+        }
+    }
 }
 
 int main()
 {
-    char str[] = "   hello world   ";
+    char str[] = "     hello world  ";
 
     myStringTrim(str);
 
